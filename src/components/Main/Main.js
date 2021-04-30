027cf44ca90f222943a8065d63bedecba957f28a
Melis <59087686+melis@users.noreply.github.com>
2021-04-30 23:50:57 +0600

    lazy_2

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,10 @@ import React, { Suspense } from "react";
 // import Catalog from "../Catalog/Catalog";
 import Faq from "../FAQ/Faq";
 import InfoBlock from "../InfoBlock/InfoBlock";
-import News from "../News/News";
+// import News from "../News/News";
 import Palayer from "../Player/Player";
 const Catalog = React.lazy(() => import("../Catalog/Catalog"));
-
+const News = React.lazy(() => import("../News/News"));
 const Main = () => {
   return (
     <>
@@ -15,7 +15,9 @@ const Main = () => {
         <Catalog />
       </Suspense>
       <Faq />
-      <News />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <News />
+      </Suspense>
     </>
   );
 };
