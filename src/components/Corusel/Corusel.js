211ae9208f8ef70e7b80d9dfdf64377ddbfecee7
Melis <59087686+melis@users.noreply.github.com>
2021-05-01 15:08:19 +0600

    car

--- src/components/Corusel/Corusel.js
+++ src/components/Corusel/Corusel.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import React, { useState, Suspense } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import style from "./Corusel.module.scss";
+// import Carousel from "react-bootstrap/Carousel";
+const Carousel = React.lazy(() => import("react-bootstrap/Carousel"));
 
 export default function CaruselB({ imgs }) {
   const [index, setIndex] = useState(0);
@@ -10,14 +12,16 @@ export default function CaruselB({ imgs }) {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      {imgs.map((img) => {
-        return (
-          <Carousel.Item key={img}>
-            <img src={img} alt="" className={style.img} />
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {imgs.map((img) => {
+          return (
+            <Carousel.Item key={img}>
+              <img src={img} alt="" className={style.img} />
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </Suspense>
   );
 }
